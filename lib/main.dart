88be6c042190384118fb7f11283154338f7d0635
mohammedashraf16 <mohammedashraf1692003@gmail.com>
2026-01-08 07:34:07 +0200

    refactor: simplify registration view.

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:markety/config/route_generator.dart';
 import 'package:markety/config/routes.dart';
 import 'package:markety/core/utils/app_colors.dart';
-import 'package:markety/core/utils/constants.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Supabase.initialize(
-    url: Constants.projectUrl,
-    anonKey: Constants.anonKey,
-  );
   runApp(const MyApp());
 }
 
